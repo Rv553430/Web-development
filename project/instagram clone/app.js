8fe72d9e34664391c68f25e03857089c9a8b08bc
Raj Verma <rv553430@gmail.com>
2024-01-29 23:09:40 +0530

    fix the javascript

--- project/instagram clone/app.js
+++ project/instagram clone/app.js
@@ -1,14 +1,28 @@
-let arr =[];
-let login_btn = document.querySelector("login-btn");
-let pass = document.querySelector("#msg");
-pass.addEventListener("input" ,function(){
-    arr.push(this.value);
-    if (arr.length>= 8)
+// let arr =[];
+function passlen()
+{
+    let login_btn = document.querySelector("#submitbtn");
+    let pass = document.querySelector("#msg");
+    console.log(login_btn);
+    if(pass.value.length >= 8)
     {
-     login_btn.disabled =false;
-     return ;
-     console.log("ho");
+        login_btn.removeAttribute("disabled");
+    }
+    else
+    {
+        login_btn.setAttribute('disabled','true');
     }
 }
-)
+
+// pass.addEventListener("input" ,function(){
+//     arr.push(this.value);
+//     if (arr.length>= 8)
+//     {
+//      login_btn.disabled =false;
+//      return ;
+//      console.log("ho");
+//     }
+// }
+// )
+
 
